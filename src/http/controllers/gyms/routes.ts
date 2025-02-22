85a804475452f6f0aeb6bd6a1d2eb0abbe094495
joschonarth <joschonarth@gmail.com>
2025-02-22 20:18:14 -0300

    feat: implement search gyms controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,9 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms', create)
+
+  app.get('/gyms/search', search)
 }
