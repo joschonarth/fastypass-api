44b9929fb1bf9420f783ef4f30342fddbd9dbb7d
joschonarth <joschonarth@gmail.com>
2025-02-22 20:15:22 -0300

    feat: implement create gym controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
+import { create } from './create'
+
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms', create)
 }
