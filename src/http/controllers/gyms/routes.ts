c860511d6e7b8a7eca269da9e34ba0a0ae26f5a4
joschonarth <joschonarth@gmail.com>
2025-02-22 20:18:49 -0300

    feat: implement search nearby gyms controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,6 +4,7 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create'
 import { search } from './search'
+import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -11,4 +12,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.post('/gyms', create)
 
   app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }
