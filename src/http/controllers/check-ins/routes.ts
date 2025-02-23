578376390fc9254a464aaced5da1a9eb370ec631
joschonarth <joschonarth@gmail.com>
2025-02-22 22:46:23 -0300

    feat: implement check-in history controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,9 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create'
+import { history } from './history'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
+
+  app.get('/check-ins/history', history)
 }
